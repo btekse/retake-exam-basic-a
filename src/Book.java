6497f4a75f3a5b62916480b74464a482eb4a1943
btekse <btekse@gmail.com>
2017-04-20 14:11:19 +0200

    add(app) : Book

---
+++ src/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+  private String author;
+  private String title;
+  private int releaseYear;
+
+  public Animal(String author, String title, int releaseYear) {
+    this.author = author;
+    this.title = title;
+    this.releaseYear = releaseYear;
+  }
+
+}
