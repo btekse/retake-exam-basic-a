03db2be44afa39390abd87c02f3095a031f2d7c8
btekse <btekse@gmail.com>
2017-04-20 11:56:41 +0200

    add(test): MinMaxDiff done

---
+++ Test/MinMaxDiffTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Balázs on 2017. 04. 20..
+ */
+public class MinMaxDiffTest {
+  MinMaxDiff minMaxDiff = new MinMaxDiff();
+
+  @Test
+  public void getMin() throws Exception {
+    ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList());
+    numberList.add(10);
+    assertEquals(10, minMaxDiff.getMin(numberList));
+  }
+
+  @Test
+  public void getMax() throws Exception {
+    ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList());
+    numberList.add(-10);
+    assertEquals(-10, minMaxDiff.getMax(numberList));
+  }
+
+  @Test
+  public void empty() throws Exception {
+    ArrayList<Integer> numberList = new ArrayList<>();
+    assertEquals(Double.NaN, MinMaxDiff.getMin(numberList));
+  }
+
+}
